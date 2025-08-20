aa529d405914467584b8f47f5d7a04df954ffb2b
vitorbnr <vitorbernardotech@gmail.com>
2025-08-19 21:32:55 -0300

    project created

---
+++ src/main/java/com/vitorbnr/evngest/EvngestApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.evngest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvngestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorbnr/evngest/EvngestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.evngest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
